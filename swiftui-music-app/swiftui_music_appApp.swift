75c07d7161f30ca5b49cd989687aac6050cc5aa8
Anay <anay0@icloud.com>
2022-02-17 23:57:16 -0700

    Initial Commit

---
+++ swiftui-music-app/swiftui_music_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_music_appApp.swift
+//  swiftui-music-app
+//
+//  Created by Anay on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_music_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
